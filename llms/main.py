1fc9e2ec19e0689ebd72abf44d8b576d7d6ab31a
Demis Bellot <demis.bellot@gmail.com>
2026-04-25 01:22:24 +0800

    Bump version to 3.0.44

--- llms/main.py
+++ llms/main.py
@@ -57,7 +57,7 @@ except ImportError:
     HAS_PIL = False
 
 _ROOT = None
-VERSION = "3.0.43"
+VERSION = "3.0.44"
 DEBUG = os.getenv("DEBUG") == "1"
 MOCK = os.getenv("MOCK") == "1"
 MOCK_DIR = os.getenv("MOCK_DIR")
